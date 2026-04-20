513a4b3350803bfa3887ca6871ec82f3a19879d4
whatabast <89269007+whatabast@users.noreply.github.com>
2026-04-20 10:17:42 -0700

    Create if filter to tell who wins

--- js.js
+++ js.js
@@ -34,17 +34,48 @@ function getHumanChoice()
     let scissors = "scissors";
 
     let x = Math.random();
+    let comp = "";
 
     if(x<=0.3)
     {
-        return rock + hand;
+        comp = rock;
+        // return rock + hand;
     }
     else if(x>0.3 && x<=0.6)
     {
-        return paper + hand;
+        comp = paper;
+        // return paper + hand;
     }
     else
     {
-        return scissors + hand;
+        comp = scissors;
+        // return scissors + hand;
+    }
+
+    let score = 0;
+
+    if(hand == comp)
+    {
+        score = score+0;
+        return "Tie! " + score
+    }
+    else if(comp==rock && hand=="scissors")
+    {
+        score = score+1;
+        return score + " Comp win rock"
+    }
+    else if(comp==paper && hand=="rock")
+    {
+        score = score + 1;
+        return score + " Comp win paper"
+    }
+        else if(comp==scissors && hand=="paper")
+    {
+        score = score + 1;
+        return score + " Comp win scissors"
+    }
+    else
+    {
+        return "Human wins! "
     }
 }
