296b97b71c8a56a783759f75ae7002d0aeffcc5d
whatabast <89269007+whatabast@users.noreply.github.com>
2026-04-20 10:02:33 -0700

    Create human input function Add computer part of function Create output to include both human and comp results

--- js.js
+++ js.js
@@ -1,7 +1,34 @@
 
 
-function getComputerChoice()
+// function getComputerChoice()
+// {
+//     let rock = "rock";
+//     let paper = "paper";
+//     let scissors = "scissors";
+
+//     let x = Math.random();
+
+//     if(x<=0.3)
+//     {
+//         return rock;
+//     }
+//     else if(x>0.3 && x<=0.6)
+//     {
+//         return paper;
+//     }
+//     else
+//     {
+//         return scissors;
+//     }
+// }
+
+
+function getHumanChoice()
 {
+    let hand = prompt("Rock! Paper!Scissors!");
+
+    // return hand;
+
     let rock = "rock";
     let paper = "paper";
     let scissors = "scissors";
@@ -10,16 +37,14 @@ function getComputerChoice()
 
     if(x<=0.3)
     {
-        return rock;
+        return rock + hand;
     }
     else if(x>0.3 && x<=0.6)
     {
-        return paper;
+        return paper + hand;
     }
     else
     {
-        return scissors;
+        return scissors + hand;
     }
-
-
 }
