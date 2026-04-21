80e891d3a5f761355fdebb6b849eac447c6c18f5
whatabast <89269007+whatabast@users.noreply.github.com>
2026-04-21 12:59:42 -0700

    Restructure to be multiple functions instead of one long one

---
+++ rps.js
@@ -0,0 +1,89 @@
+// ---------------------------
+function getComputerChoice()
+{
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+    let compChoice = "";
+
+    let rand = Math.random();
+
+    if(rand<=0.3)
+    {
+        compChoice = rock;
+        return compChoice;
+    }
+    else if(rand>0.3 && rand<=0.6)
+    {
+        compChoice = paper;
+        return compChoice;
+    }
+    else
+    {
+        compChoice = scissors;
+        return compChoice;
+    }
+
+    // console.log(compChoice)
+}
+
+let computer = getComputerChoice()
+console.log(computer)
+
+// -------------------------------
+
+function getHumanChoice(rps)
+{
+    return rps.toLowerCase();
+}
+let human = getHumanChoice(prompt("Rock! Paper! Scissors"));
+
+console.log(human);
+// ------------------------------
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice)
+{
+    if(humanChoice == computerChoice)
+    {
+        console.log("Tie!") 
+    }
+    else if(computerChoice=="rock" && humanChoice=="scissors")
+    {
+        computerScore=computerScore+1;
+        console.log("Computer wins! Rock beats Scissors")
+    }
+    else if(computerChoice=="paper" && humanChoice=="rock")
+    {
+        computerScore=computerScore+1;
+        console.log("Computer wins! Paper beats Rock")
+    }
+        else if(computerChoice=="scissors" && humanChoice=="paper")
+    {
+        computerScore=computerScore+1;
+        console.log("Computer wins! Scissors beats Paper")
+    }
+    else
+    {
+        humanScore = humanScore+1;
+        console.log("Human Wins! " + human + " beats " + computer);
+    }
+
+    console.log("Computer: " + computerScore 
+        + " Human: " + humanScore)
+        
+}
+
+
+// -------------------------------------------------
+
+function playGame()
+{
+    for(let round = 0; round<5; round++)
+    {
+        playRound(human, computer) 
+    }
+}
+
+playGame();
