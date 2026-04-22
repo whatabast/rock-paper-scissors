1f45184bdb1a2c777c0d03458b38c411bc70f79f
whatabast <89269007+whatabast@users.noreply.github.com>
2026-04-22 14:50:09 -0700

    Add divRound, divScore and divFinal to be able to out put multiple new divs Old way only had one div and would overwrite eachother

--- rps.js
+++ rps.js
@@ -1,5 +1,7 @@
 const body = document.body;
-const div = document.createElement("div");
+const divRound = document.createElement("div");
+const divScore = document.createElement("div");
+const divFinal = document.createElement("div");
 
 
 // ---------------------------
@@ -43,36 +45,37 @@ function playRound(humanChoice, computerChoice)
 {
     if(humanChoice == computerChoice)
     {
-        div.textContent = "Tie!";
-        body.append(div);
+        divRound.textContent = "Tie!";
+        body.append(divRound);
     }
     else if(computerChoice=="rock" && humanChoice=="scissors")
     {
         computerScore=computerScore+1;
-        div.textContent = "Computer wins! Rock beats Scissors";
-        body.append(div);
+        divRound.textContent = "Computer wins! Rock beats Scissors";
+        body.append(divRound);
     }
     else if(computerChoice=="paper" && humanChoice=="rock")
     {
         computerScore=computerScore+1;
-        div.textContent = "Computer wins! Paper beats Rock";
-        body.append(div);
+        divRound.textContent = "Computer wins! Paper beats Rock";
+        body.append(divRound);
     }
         else if(computerChoice=="scissors" && humanChoice=="paper")
     {
         computerScore=computerScore+1;
-        div.textContent = "Computer wins! Scissors beats Paper";
-        body.append(div);
+        divRound.textContent = "Computer wins! Scissors beats Paper";
+        body.append(divRound);
     }
     else
     {
         humanScore = humanScore+1;
-        div.textContent =         "Human Wins! " + human + " beats " + computer;
-        body.append(div);
+        divRound.textContent = "Human Wins! " + human + " beats " + computer;
+        body.append(divRound);
     }
 
-        console.log("Computer: " + computerScore 
-            + " Human: " + humanScore)
+        divScore.textContent = "Computer: " + computerScore 
+            + " Human: " + humanScore;
+        body.append(divScore);
 
 
 }
